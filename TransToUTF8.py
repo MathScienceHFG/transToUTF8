349eb350abe45a9d5c7d2908bd07f76c99cdd1e7
Legong Sun <legong.sun@gmail.com>
2016-05-03 00:53:41 +0900

    using option '-w -Lw --overwrite' of ukf32.exe instead mid files

--- TransToUTF8.py
+++ TransToUTF8.py
@@ -2,12 +2,11 @@
 
 ##
 #   @file       TransToUTF8.py
-#   @brief      递归搜索指定目录，将所有指定类型的文件，编码为UTF8，保持原名。
+#   @brief      递归搜索指定目录，将所有指定类型的文件，编码为UTF8(No BOM)+windows改行形式(CRLF)，并保持原名。
 #
 #   @author     legong.sun@gmail.com
 #   @date       2016-05-02
 #
-
 
 import os
 import sys
@@ -20,18 +19,20 @@ fileType = '.' + sys.argv[2]    # 指定文件类型， eg：cpp, hpp, py
 
 codeType = '.utf8'
 srcDirList = fm.findFiles(rootDir, fileType)                    # src files:  *.fileType
-midDirList = fm.findFiles(rootDir, codeType)                    # tmp files:  *.fileType.codeType
-
-# clean existed mid files
-for midDir in midDirList:
-    os.system("rm -f {}".format(midDir))
-    print "removed", midDir
 
 # trans all src files into dst type, with same names
 for srcDir in srcDirList:
-    dstDir = srcDir                                             # eg. *.cpp
-    midDir = srcDir + codeType                                  # eg. *.cpp.utf8
-    os.system("nkf32.exe -w {} > {}".format(srcDir, midDir))    # eg. *cpp > *.cpp.utf8
-    os.system("rm -f {}".format(srcDir))                        # eg. rm -f *cpp
-    os.system("mv {} {}".format(midDir, dstDir))                # eg. mv *cpp.utf8 *.cpp
+    # using nkf32.exe to trans the file
+    # download from: http://www.vector.co.jp/soft/win95/util/se295331.html
+    os.system("nkf32.exe -w -Lw --overwrite {}".format(srcDir))
     print "finished", srcDir
+    # -j            :JISコード(ISO-2022-JP)を出力(省略可能)
+    # -e            :EUCコードを出力
+    # -s            :Shift-JISコードを出力
+    # -w            :UTF-8コードを出力（BOM無し）
+    # -Lu           :unix改行形式(LF)に変換
+    # -Lw           :windows改行形式(CRLF)に変換
+    # -Lm           :macintosh改行形式(CR)に変換
+    # -g(--guess)   :自動判別の結果を表示
+    # --overwrite   :引数のファイルに直接上書き
+    # --version     :バージョン情報を表示
